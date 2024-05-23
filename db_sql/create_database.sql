e03ca941780181b059376bc38f78d03ed42701df
Pedro de Sá <pedrodesa42@gmail.com>
2024-05-22 22:36:53 -0300

    Arquivo para criação de banco de dados

---
+++ db_sql/create_database.sql
@@ -0,0 +1,15 @@
+-- Criar banco de dados
+-- DB SINAN
+CREATE DATABASE IF NOT EXISTS sinan_hansen;
+
+
+-- DB IBGE
+CREATE DATABASE IF NOT EXISTS penad_ibge;
+
+
+-- DB Modelo 1
+CREATE DATABASE IF NOT EXISTS modelo_preditivo;
+
+
+-- DB Modelo 2
+CREATE DATABASE IF NOT EXISTS modelo_cluster;
